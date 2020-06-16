a2a8c653f8aca1cbb0922c7ed663f6011561442c
ely <ely137137@gmail.com>
2020-06-16 18:23:37 +0300

    Меняет подключение полифиллов

--- source/js/script.js
+++ source/js/script.js
@@ -3,5 +3,7 @@
 import {load} from "./components/backend";
 import {renderCards} from "./components/cards";
 import {renderError} from "./components/error";
+import {setPolyfills} from "./components/polyfills";
 
+setPolyfills();
 load(renderCards, renderError);
